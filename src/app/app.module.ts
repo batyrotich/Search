06ac6ec31b94e4720b56c4f415b588da33407798
eddah <eddahbaty98@gmail.com>
2020-08-30 19:43:20 +0300

    add form to the userprofile.html and  routed it in the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { UserprofileComponent } from './userprofile/userprofile.component';
 import { RepositoryComponent } from './repository/repository.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { from } from 'rxjs';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +18,9 @@ import { RepositoryComponent } from './repository/repository.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
