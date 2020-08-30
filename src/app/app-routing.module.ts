7b06104313b88994435439c2d07a9820af9146ea
eddah <eddahbaty98@gmail.com>
2020-08-30 12:51:02 +0300

    add new repositories, userprofile and repository directories and added to the routing module.ts file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { UserprofileComponent } from './userprofile/userprofile.component'
+import { RepositoryComponent } from './repository/repository.component'
+            
 const routes: Routes = [];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { UserprofileComponent } from './userprofile/userprofile.component';
+import { RepositoryComponent } from './repository/repository.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    UserprofileComponent,
+    RepositoryComponent
   ],
   imports: [
     BrowserModule,
